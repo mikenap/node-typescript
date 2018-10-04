7df41fe4508d01ce4cb6363942f5f1af7a4af961
mikenap <manapoleoni@gmail.com>
2018-10-04 19:55:30 -0400

    updated npm scripts, nodemon, concurrently

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import express from 'express';
-import { Request, Response } from 'express';
-
-const app = express();
-const port = 3333;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, from my TypeScript Express app!');
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+console.log('Hello, World');
